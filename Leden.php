d6946f840b907d0160c3be0c14b258f5ca6a135b
Rugtert <rutgerklomp@gmail.com>
2019-04-24 16:50:07 +0200

    supahhh

--- Leden.php
+++ Leden.php
@@ -1,59 +1,51 @@
 <?php include "./templates/header.php"; ?>
-    <div class="container">
-        <div class="table-wrapper">
-            <div class="table-title">
-                <div class="row">
-                    <div class="col-sm-6">
-						<h2><b>Overzicht leden</b></h2>
-					</div>
-                </div>
+<?php
+	require "./config/configsql.php";
+	// Create connection
+	$conn = mysqli_connect($host, $username, $password, $dbname);
+	// Check connection
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	};
+	$sql = "SELECT lid.lid_nr, voornaam, achternaam FROM lid";
+	$result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) < 1) {
+		die("no results");
+	};
+?>
+<div class="container">
+    <div class="table-wrapper">
+        <div class="table-title">
+            <div class="row">
+                <div class="col-sm-6">
+					<h2><b>Overzicht leden</b></h2>
+				</div>
             </div>
-            <table class="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>Lid nummer</th>
-                        <th>Voornaam</th>
-						<th>Achternaam</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-					<?php
-						require "./config/configsql.php";
-                        // Create connection
-                        $conn = mysqli_connect($host, $username, $password, $dbname);
-                        // Check connection
-                        if (!$conn) {
-                            die("Connection failed: " . mysqli_connect_error());
-						};
-						$sql = "SELECT lid.lid_nr, voornaam, achternaam FROM lid";
-						$result = mysqli_query($conn, $sql);
-						if (mysqli_num_rows($result) > 0) {
-							// output data of each row
-							while($row = mysqli_fetch_assoc($result)) {
-								echo "<tr><td>". $row["lid_nr"]. "</td>" . "<td>" . $row["voornaam"]. "</td>" . "<td>" . $row["achternaam"]. "</td>" . 
-									"<td>" . "
-									<form action=Lid_Aanpassen.php method=\"post\">
-										<input type=\"Hidden\" Name=\"lid_nr\" Value=" . $row["lid_nr"] . ">
-										<input type=\"Submit\" Value=\"Aanpassen\">
-									</form>
-									</td>	
-									<td>
-									<form action=Lid_Verwijderen.php method=\"post\">
-										<input type=\"Hidden\" Name=\"lid_nr\" Value=" . $row["lid_nr"] . ">
-										<input type=\"Submit\" Value=\"Verwijderen\">
-									</form>	
-									</td>
-								</tr>";
-							};
-							mysqli_close($conn);
-							} else {
-								echo "0 results";
-							}
-						?>
-                </tbody>
-            </table>
         </div>
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Lid nummer</th>
+                    <th>Voornaam</th>
+					<th>Achternaam</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+			<?php
+				// output data of each row
+				foreach ($result as $row) :
+			?>
+			<tr>
+				<td><?php echo $row["lid_nr"];?></td>
+				<td><?php echo $row["voornaam"];?></td>
+				<td><?php echo $row["achternaam"];?></td> 
+				<td><a href="Lid_aanpassen.php?lid_nr=<?php echo $row["lid_nr"];?>">Aanpassen</a></td>
+			</tr>
+			<?php endforeach ?>
+            </tbody>
+        </table>
     </div>
+</div>
 <?php include "./Templates/footer.php"; ?>
--- Lid_Aanpassen.php
+++ Lid_Aanpassen.php
@@ -1,43 +1,103 @@
 <?php include "./templates/header.php"; ?>
+<?php 
+    require "./config/configsql.php";
 
+    if (isset($_GET['lid_nr'])) {
+        echo $_GET['lid_nr']; //test doeleinde...
+    }
+    else {
+        Echo "Something went wrong";
+        exit;
+    };
+
+?>
+<?php /*<div class="container">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-6">
+						<h2><b>Overzicht leden</b></h2>
+					</div>
+                </div>
+            </div>
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Lid nummer</th>
+                        <th>Voornaam</th>
+						<th>Achternaam</th>
+                        <th>straatnaam</th>
+                        <th>huisnummer</th>
+                        <th>woonplaats</th>
+                        <th>postcode</th>
+                        <th>emailadres</th>
+                    </tr>
+                </thead>
+                <tbody>
 
 <?php
+//verwerken formulier
+    if (isset($_POST['submit'])) {
+        Require "./config/configsql.php";
+        $conn = mysqli_connect($host, $username, $password, $dbname);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        $sql = "UPDATE lid SET voornaam = $_POST[voornaam] achternaam = $_POST[achternaam] straatnaam = $_POST[straatnaam] huisnummer = $_POST[huisnummer] woonplaats = $_POST[woonplaats] postcode = $_POST[postcode] emailadres = $_POST[emailadres] 
+                WHERE lid_nr = $_POST[lid_nr]";
+        echo $sql;
+    }
+?>
+<?php
+//Input formulier
 Require "./config/configsql.php";
     $lid_nr = $_POST['lid_nr'];
-    echo "$lid_nr" ;
     // Create connection
     $conn = mysqli_connect($host, $username, $password, $dbname);
     // Check connection
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
-    $sql = "SELECT Lid_nr,Voorletter,voornaam,voorvoegsel,achternaam,straatnaam,huisnummer,huisnummertoevoeging,woonplaats,postcode,telefoonnummer,emailadres,geboortedatum FROM lid 
+    $sql = "SELECT lid_nr,voorletter,voornaam,voorvoegsel,achternaam,straatnaam,huisnummer,huisnummertoevoeging,woonplaats,postcode,telefoonnummer,emailadres,geboortedatum FROM lid 
             WHERE lid_nr = $lid_nr";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
         // output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
-            echo "<tr><td>". $row["lid_nr"]. "</td>" . "<td>" . $row["voornaam"]. "</td>" . "<td>" . $row["achternaam"]. "</td>" . 
-            "<td>" . "
-            <form action=Lid_Aanpassen.php method=\"post\">
-                <input type=\"Hidden\" Name=\"lid_nr\" Value=" . $row["lid_nr"] . ">
-                <input type=\"Submit\" Value=\"Aanpassen\">
-            </form>
-            </td>	
-            <td>
-            <form action=Lid_Verwijderen.php method=\"post\">
-                <input type=\"Hidden\" Name=\"lid_nr\" Value=" . $row["lid_nr"] . ">
-                <input type=\"Submit\" Value=\"Verwijderen\">
-            </form>	
-            </td>
-        </tr>";
-    };;
-        }
-    } else {
+        $row = mysqli_fetch_assoc($result) ;
+            $lid_nr                 = $row["lid_nr"];
+            $voornaam               = $row["voornaam"];
+            $achternaam             = $row["achternaam"];
+            $straatnaam             = $row["straatnaam"];
+            $huisnummer             = $row["huisnummer"];
+            $woonplaats             = $row["woonplaats"];
+            $postcode               = $row["postcode"];
+            $emailadres             = $row["emailadres"];
+            $geboortedatum          = $row["geboortedatum"];
+            //$Voorletter             = $row["voorletter"];
+            //$voorvoegsel            = $row["voorvoegsel"];
+            //$telefoonnummer         = $row["telefoonnummer"];
+            //$huisnummertoevoeging   = $row["huisnummertoevoeging"];
+            "<form method=\"post\">";
+            echo "<tr>" . "
+                    <td><input type=\"text\" Name=\"lid_nr\" Value=\"" . $lid_nr. "\"readonly size=\"4\"></td>
+                    <td><input type=\"text\" Name=\"voornaam\" Value=\"" . $voornaam. "\" size=\"14\"></td>
+                    <td><input type=\"text\" Name=\"achternaam\" Value=\"" . $achternaam. "\" size=\"14\"></td>
+                    <td><input type=\"text\" Name=\"straatnaam\" Value=\"" . $straatnaam. "\" size=\"14\"></td>
+                    <td><input type=\"text\" Name=\"huisnummer\" Value=\"" . $huisnummer. "\" size=\"4\"></td>
+                    <td><input type=\"text\" Name=\"woonplaats\" Value=\"" . $woonplaats. "\" size=\"14\"></td>
+                    <td><input type=\"text\" Name=\"postcode\" Value=\"" . $postcode. "\" size=\"6\"></td>
+                    <td><input type=\"text\" Name=\"emailadres\" Value=\"" . $emailadres. "\" size=\"14\"></td>
+                </tr></table>
+                <input type=\"submit\" Name=\"submit\" Value=\"Lidgegevens aanpassen\">
+                </form>";
+        
+    }
+    else {
         echo "0 results";
     }
 
     mysqli_close($conn);
 ?>
-<?php include "./templates/footer.php"; ?>
+<?php include "./templates/footer.php"; ?>*/
