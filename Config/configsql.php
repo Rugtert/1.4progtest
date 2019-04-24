845279cf0ce101a32a796fab082f4ad24f8d5826
Rugtert <rutgerklomp@gmail.com>
2019-04-24 10:56:37 +0200

    reeeeee

--- Config/configsql.php
+++ Config/configsql.php
@@ -1,7 +1,7 @@
 <?php
-//Parameters gebruikt om databaseverbinding op te zetten
-$host = "localhost";
-$username = "root";
-$password ="";
-$dbname ="debieb";
+    //Parameters gebruikt om databaseverbinding op te zetten via PDO
+    $host       = "localhost";
+    $username   = "root";
+    $password   = "";
+    $dbname     = "debieb";
 ?>
