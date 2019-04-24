4abab64f0392943f31090d9c95d67b47a9f1b65e
Rugtert <rutgerklomp@gmail.com>
2019-04-24 10:57:02 +0200

    re

--- Lid_Aanpassen.php
+++ Lid_Aanpassen.php
@@ -3,11 +3,6 @@
 
 <?php
 Require "./config/configsql.php";
-    //function liddetails
-    #$servername = "localhost";
-    #$username = "root";
-    #$password = "";
-    #$dbname = "debieb";
     $lid_nr = $_POST['lid_nr'];
     echo "$lid_nr" ;
     // Create connection
@@ -16,7 +11,6 @@ Require "./config/configsql.php";
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
-
     $sql = "SELECT Lid_nr,Voorletter,voornaam,voorvoegsel,achternaam,straatnaam,huisnummer,huisnummertoevoeging,woonplaats,postcode,telefoonnummer,emailadres,geboortedatum FROM lid 
             WHERE lid_nr = $lid_nr";
     $result = mysqli_query($conn, $sql);
@@ -24,7 +18,21 @@ Require "./config/configsql.php";
     if (mysqli_num_rows($result) > 0) {
         // output data of each row
         while($row = mysqli_fetch_assoc($result)) {
-            "<form> ";
+            echo "<tr><td>". $row["lid_nr"]. "</td>" . "<td>" . $row["voornaam"]. "</td>" . "<td>" . $row["achternaam"]. "</td>" . 
+            "<td>" . "
+            <form action=Lid_Aanpassen.php method=\"post\">
+                <input type=\"Hidden\" Name=\"lid_nr\" Value=" . $row["lid_nr"] . ">
+                <input type=\"Submit\" Value=\"Aanpassen\">
+            </form>
+            </td>	
+            <td>
+            <form action=Lid_Verwijderen.php method=\"post\">
+                <input type=\"Hidden\" Name=\"lid_nr\" Value=" . $row["lid_nr"] . ">
+                <input type=\"Submit\" Value=\"Verwijderen\">
+            </form>	
+            </td>
+        </tr>";
+    };;
         }
     } else {
         echo "0 results";
